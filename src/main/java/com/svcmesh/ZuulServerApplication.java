29e430d839511a50dcf3ed84bc4b7fac25af6504
hiondal <hiondal@gmail.com>
2020-11-04 19:04:39 +0900

    aa

--- src/main/java/com/svcmesh/ZuulServerApplication.java
+++ src/main/java/com/svcmesh/ZuulServerApplication.java
@@ -2,10 +2,12 @@ package com.svcmesh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableZuulProxy
 public class ZuulServerApplication {
 
