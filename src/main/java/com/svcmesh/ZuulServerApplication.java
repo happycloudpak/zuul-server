3638489559500edf77860a1e86b3b71fc757ca6b
hiondal <hiondal@gmail.com>
2020-11-04 10:42:00 +0900

    initial

---
+++ src/main/java/com/svcmesh/ZuulServerApplication.java
@@ -0,0 +1,20 @@
+package com.svcmesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+}
